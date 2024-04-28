5642809ad1e6288c34318022cfd97d3c29cebbb0
lyh5426 <lyh5426@naver.com>
2024-04-28 20:13:24 +0900

    [24.04.28] PermActivity 권한설정 프로세스 완료

---
+++ app/src/main/java/com/yunho/king/Utils/PermManager.kt
@@ -0,0 +1,92 @@
+package com.yunho.king.Utils
+
+import android.app.AppOpsManager
+import android.app.AppOpsManager.MODE_ALLOWED
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.Settings
+import androidx.core.content.ContextCompat
+
+class PermManager(context: Context) {
+    private val mContext: Context = context
+    private val permArr = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            android.Manifest.permission.POST_NOTIFICATIONS,
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.RECORD_AUDIO
+        )
+    } else {
+        arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.RECORD_AUDIO
+        )
+    }
+
+    fun isAllPermAllow(): Boolean {
+        if (!(isOverlayAllow() && isUsagesPermAllow())) return false
+
+        for (i in permArr) {
+            if (ContextCompat.checkSelfPermission(mContext, i)
+                != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun isRuntimePermAllow(): Boolean {
+        for (i in permArr) {
+            if (ContextCompat.checkSelfPermission(mContext, i)
+                != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun isPermAllow(permName: String): Boolean =
+        ContextCompat.checkSelfPermission(mContext, permName) == PackageManager.PERMISSION_GRANTED
+
+    fun isNowPermAllow(permIndex: Int): Boolean {
+        val permName = permArr[permIndex]
+
+        return ContextCompat.checkSelfPermission(
+            mContext,
+            permName
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun isNowPerm(permIndex: Int): String = permArr[permIndex]
+
+    fun permSize(): Int = permArr.size-1
+
+    fun isUsagesPermAllow(): Boolean {
+        val appOps = mContext.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                appOps.unsafeCheckOpNoThrow(
+                    OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(),
+                    mContext.packageName
+                )
+            } else {
+                appOps.checkOpNoThrow(
+                    OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), mContext.packageName)
+            }
+        } catch (exception: SecurityException) {
+            9999
+        }
+
+        return if(mode == AppOpsManager.MODE_DEFAULT) {
+            ContextCompat.checkSelfPermission(
+                mContext,
+                android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED
+        } else {
+            mode == MODE_ALLOWED
+        }
+    }
+
+    fun isOverlayAllow(): Boolean = Settings.canDrawOverlays(mContext)
+}
---
+++ app/src/main/java/com/yunho/king/presentation/ui/perm/PermActivity.kt
@@ -0,0 +1,142 @@
+package com.yunho.king.presentation.ui.perm
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.yunho.king.Utils.PermManager
+import com.yunho.king.databinding.ActivityPermBinding
+import com.yunho.king.presentation.ui.base.BaseActivity
+import com.yunho.king.presentation.ui.intro.IntroActivity
+
+class PermActivity : BaseActivity() {
+
+    lateinit var binding: ActivityPermBinding
+    lateinit var firstPermissionLauncher: ActivityResultLauncher<String>
+    lateinit var secondPermissionLauncher: ActivityResultLauncher<String>
+    lateinit var permissionSettingLauncher: ActivityResultLauncher<Intent>
+    lateinit var overlayLauncher: ActivityResultLauncher<Intent>
+    lateinit var usagesPermLauncher: ActivityResultLauncher<Intent>
+    lateinit var permManager: PermManager
+
+    var permIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPermBinding.inflate(layoutInflater)
+
+        permManager = PermManager(this)
+
+        firstPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                binding.btnPermStart.text = "13241"
+
+                if (permIndex >= permManager.permSize()) {
+                    if (permManager.isRuntimePermAllow()) {
+                        moveToOverlayPerm()
+                    } else {
+                        permIndex = 0
+                        secondPermissionLauncher.launch(permManager.isNowPerm(permIndex))
+                    }
+                } else {
+                    permIndex += 1
+                    firstPermissionLauncher.launch(permManager.isNowPerm(permIndex))
+                }
+            }
+
+        secondPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                if (permIndex >= permManager.permSize()) {
+                    if (permManager.isRuntimePermAllow()) {
+                        moveToOverlayPerm()
+                    } else {
+                        moveToApplicationDetail()
+                    }
+                } else {
+                    permIndex += 1
+                    secondPermissionLauncher.launch(permManager.isNowPerm(permIndex))
+                }
+            }
+
+        permissionSettingLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (permManager.isRuntimePermAllow()) {
+                    moveToOverlayPerm()
+                } else {
+                    moveToApplicationDetail()
+                }
+            }
+
+        overlayLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (permManager.isOverlayAllow()) {
+                    moveToUsagesPerm()
+                } else {
+                    moveToOverlayPerm()
+                }
+
+            }
+
+        usagesPermLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (permManager.isUsagesPermAllow()) {
+                    moveToIntent(
+                        Intent(this, IntroActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        }
+                    )
+                } else {
+                    moveToUsagesPerm()
+                }
+            }
+        registOnclickListener()
+        setContentView(binding.root)
+    }
+
+    fun registOnclickListener() = with(binding) {
+        btnPermStart.setOnClickListener {
+//            txt.text = "bbbb"
+            startPermAllow()
+        }
+
+        cccc.setOnClickListener {
+            txt.text = "cccc"
+//            startPermAllow()
+        }
+    }
+
+    fun startPermAllow() {
+        firstPermissionLauncher.launch(permManager.isNowPerm(permIndex))
+    }
+
+    fun moveToApplicationDetail() {
+        permissionSettingLauncher.launch(
+            Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:${packageName}"))
+        )
+    }
+
+    fun moveToOverlayPerm() {
+        overlayLauncher.launch(
+            Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:${packageName}")
+            )
+        )
+    }
+
+    fun moveToUsagesPerm() {
+        usagesPermLauncher.launch(
+            Intent(
+                Settings.ACTION_USAGE_ACCESS_SETTINGS,
+                Uri.parse("package:${packageName}"))
+        )
+    }
+}
