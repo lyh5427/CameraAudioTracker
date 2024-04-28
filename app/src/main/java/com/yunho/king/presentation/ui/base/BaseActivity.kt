751470d2957b01ab3b6f35b8605b6e06ec54b689
lyh5426 <lyh5426@naver.com>
2024-04-28 20:13:24 +0900

    [24.04.28] PermActivity 권한설정 프로세스 완료

--- app/src/main/java/com/yunho/king/presentation/ui/base/BaseActivity.kt
+++ app/src/main/java/com/yunho/king/presentation/ui/base/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.yunho.king.presentation.ui.base
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yunho.king.R
@@ -8,4 +9,9 @@ open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    fun moveToIntent(intent: Intent) {
+        startActivity(intent)
+    }
+
 }
