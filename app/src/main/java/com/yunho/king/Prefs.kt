fad3535650768c2074c3c8bfda06d9b7097e5299
lyh5426 <lyh5426@naver.com>
2024-04-28 19:50:24 +0900

    [24.04.28] First Commit

---
+++ app/src/main/java/com/yunho/king/Prefs.kt
@@ -0,0 +1,16 @@
+package com.yunho.king
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Prefs (context : Context) {
+    val PREFS_FILENAME = "com.yunho.kinh.prefs"
+    val prefs: SharedPreferences = context.getSharedPreferences(
+        PREFS_FILENAME,
+        Context.MODE_PRIVATE
+    )
+
+    var firstPermissionCheck: Boolean
+        get() = prefs.getBoolean("firstPermissionCheck", true)
+        set(value) = prefs.edit().putBoolean("firstPermissionCheck", value).apply()
+}
---
+++ app/src/main/java/com/yunho/king/presentation/ui/base/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.yunho.king.presentation.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yunho.king.R
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
