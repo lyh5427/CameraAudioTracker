682650afd67294c74243750f0dfa8bd455994970
lyh5426 <lyh5426@naver.com>
2024-05-22 00:19:02 +0900

    [24.05.22] 카메라 앱 / 오디오 앱 DB저장

--- app/src/main/java/com/yunho/king/presentation/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/yunho/king/presentation/ui/intro/IntroActivity.kt
@@ -11,13 +11,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.yunho.king.BuildConfig
 import com.yunho.king.R
+import com.yunho.king.Utils.PermManager
 import com.yunho.king.databinding.ActivityIntroBinding
 import com.yunho.king.presentation.ui.base.BaseActivity
 import com.yunho.king.presentation.ui.main.MainActivity
+import com.yunho.king.presentation.ui.perm.PermActivity
 
 class IntroActivity : BaseActivity() {
 
     lateinit var binding: ActivityIntroBinding
+    val permManager = PermManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +30,38 @@ class IntroActivity : BaseActivity() {
         setSplashAnimation()
     }
 
+    override fun onResume() {
+        super.onResume()
+        startIntro()
+    }
+
     fun setSplashAnimation() = with(binding) {
         val anim = AnimationUtils.loadAnimation(this@IntroActivity, R.anim.anim_appear)
 
         binding.splash.startAnimation(anim)
+    }
+
+    private fun startIntro() {
+        if (permManager.isAllPermAllow()) {
+            insertCameraPackage()
+            insertAudioPackage()
+            navigateToMain()
+        } else {
+            navigateToPerm()
+        }
+    }
+
+    private fun navigateToPerm() {
+        startActivity(
+            Intent(this, PermActivity::class.java)
+        )
+    }
+
+    private fun navigateToMain() {
+        startActivity(
+            Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+        )
     }
 }
