ac930fa55559a52d6df2b0f390e7dec4f7e60e5c
lyh5426 <lyh5426@naver.com>
2024-08-23 01:11:56 +0900

    [24.08.23] Main 광고 추가

--- app/src/main/java/com/yunho/king/GlobalApplication.kt
+++ app/src/main/java/com/yunho/king/GlobalApplication.kt
@@ -1,7 +1,11 @@
 package com.yunho.king
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 @HiltAndroidApp
@@ -11,6 +15,13 @@ class GlobalApplication: Application() {
             prefs = Prefs(this)
 //        val dexOutputDir: File = codeCacheDir
 //        dexOutputDir.setReadOnly()
+    }
+
+    private fun setAdmob() {
+        CoroutineScope(Dispatchers.IO).launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@GlobalApplication) {}
+        }
     }
 
     companion object {
